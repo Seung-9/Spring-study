2a5511b408a53f78c4c3e2624973657ed8985ffd
Seung-9 <ssg9505fj22@naver.com>
2023-08-21 02:52:05 +0900

    refactor: AppConfig 코드 개선

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,40 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
-public class AppConfig {
-    // DI ( 의존관계 주입 )
+public class AppConfig { // DI ( 의존관계 주입 )
+
+    /* 리팩토링 전: '역할'에 따른 '구현'이 잘 안 보임
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+     */
+
+    // 리팩토링 후
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
