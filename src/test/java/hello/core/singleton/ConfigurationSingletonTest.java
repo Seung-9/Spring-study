134e84f8e481754dfafac873dc027c219427ca68
Seung-9 <ssg9505fj22@naver.com>
2023-09-02 22:09:16 +0900

    test: @Configuration과 CGLIB

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,17 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        // class hello.core.AppConfig 라고 출력되어야 하지만 다른 식으로 출력이 됨.
+        // CGLIB 라이브러리를 사용하여 AppConfig 클래스를 상속받은 다른 클래스를 만들고, 그 클래스를 스프링 빈으로 등록하기 때문임.
+        // 만들어진 새로운 클래스 로직은 이미 컨테이너에 존재하면 컨테이너에 있는 것을 반환하고, 그렇지 않으면 스프링 컨테이너에 등록하고 반환하는 로직
+        // 그러므로 AppConfig에서 call AppConfig.memberRepository 이 한 번만 실행되는 것.
+        // 만약 AppConfig 에 @Configuration 사용하지 않으면 CGLIB 라이브러리를 사용하지 않음 => 싱글톤이 꺠짐
+        System.out.println("bean = " + bean.getClass());
+    }
 }
