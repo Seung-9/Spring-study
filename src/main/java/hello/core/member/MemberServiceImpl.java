ed9f50d79289b9d073f4918994ccaeb781182417
Seung-9 <ssg9505fj22@naver.com>
2023-09-03 23:50:47 +0900

    test: 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -6,15 +6,14 @@ import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
-//    @Autowired
-//    public MemberServiceImpl(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
+    @Autowired
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
