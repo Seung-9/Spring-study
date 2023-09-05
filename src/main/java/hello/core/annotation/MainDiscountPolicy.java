4caa3dbce908708a79ff71618e144869cf50567d
Seung-9 <ssg9505fj22@naver.com>
2023-09-06 00:36:32 +0900

    chore: 주석 추가

--- src/main/java/hello/core/annotation/MainDiscountPolicy.java
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.lang.annotation.*;
 
+// @Qualifier 사용 시 문자를 넣는 것보단 어노테이션을 정의해서 쓰는 것이 좋음. 무분별하게 사용하는 것은 좋지 않음
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
