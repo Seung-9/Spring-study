28658bb9bb09d38994495b19f0103a8dfe6e6902
Seung-9 <ssg9505fj22@naver.com>
2023-09-07 01:49:28 +0900

    test: 빈 생명주기 콜백

--- src/test/java/hello/core/lifetcycle/NetworkClient.java
+++ src/test/java/hello/core/lifetcycle/NetworkClient.java
@@ -3,14 +3,15 @@ package hello.core.lifetcycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-//        connect();
-//        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -31,17 +32,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    // 의존관계 주입이 끝나면 호출
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
 
-    // 빈이 종료 될 때 호출
-    @Override
-    public void destroy() throws Exception {
+    @PreDestroy
+    public void close() {
         System.out.println("NetworkClient.destroy");
        disconnect();
     }
