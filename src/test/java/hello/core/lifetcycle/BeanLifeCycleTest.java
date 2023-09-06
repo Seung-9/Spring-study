6c7f0dffd0300d92e45e76b5a996fae8807faafe
Seung-9 <ssg9505fj22@naver.com>
2023-09-07 00:55:42 +0900

    chore: 스프링 빈 라이프사이클 관련 주석 추가

--- src/test/java/hello/core/lifetcycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifetcycle/BeanLifeCycleTest.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/*
+스프링 빈의 라이프 사이클은 간단하게 객체 생성 -> 의존관계 주입이다. 따라서 networkClient 객체가 생성된 후에 수정자 주입이 이루어지기 때문에 url = null 이다.
+
+스프링 빈의 이벤트 라이프 사이클: 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종
+ */
 public class BeanLifeCycleTest {
 
     @Test
